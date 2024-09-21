78ba82ee9365b47d0a8acc8e2308c52e70b9e56f
srCochayuyo <129439145+srCochayuyo@users.noreply.github.com>
2024-09-20 23:12:55 -0300

    Feat:dotnet,  models, framework relationships, data

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using ayudantis1.src.Data;
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Env.Load();
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+string connectionString = Environment.GetEnvironmentVariable("DATABASE_URL") ?? "Data Source=app.db";
+builder.Services.AddDbContext<ApplicationDBContext>(opt => opt.UseSqlite(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.Run();
