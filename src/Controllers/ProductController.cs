701e1a589b23cc3a69f334d83612f2849e319be2
srCochayuyo <129439145+srCochayuyo@users.noreply.github.com>
2024-09-24 20:38:14 -0300

    feat: controller migration code

--- src/Controllers/ProductController.cs
+++ src/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ayudantis1.src.Data;
 using ayudantis1.src.Dtos;
+using ayudantis1.src.Interface;
 using ayudantis1.src.Mappers;
 using ayudantis1.src.models;
 using Bogus.DataSets;
@@ -15,13 +16,14 @@ using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 namespace ayudantis1.src.Controllers
 {
     [Route("api/[controller]")]
+    [ApiController]
     public class ProductController : ControllerBase
     {
-        private readonly ApplicationDBContext _context;
-
-        public ProductController(ApplicationDBContext context)
+        
+        private readonly IProductsRepository _productRepository;
+        public ProductController(IProductsRepository productsRepository)
         {
-            _context = context;
+            _productRepository = productsRepository;
         }
 
         //metodos http
@@ -31,7 +33,7 @@ namespace ayudantis1.src.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var products = await _context.Products.ToListAsync(); //borrar
+            var products = await  _productRepository.GetAll();
             var productDto = products.Select(p => p.ToProductDto());
             return Ok(productDto);
         }
@@ -41,7 +43,7 @@ namespace ayudantis1.src.Controllers
 
         public async Task<IActionResult> GetById([FromRoute] int id)
         {
-            var product = await _context.Products.FindAsync(id);
+            var product = await _productRepository.GetById(id);
 
             if(product == null)
             {
@@ -57,8 +59,7 @@ namespace ayudantis1.src.Controllers
         public async Task <IActionResult> Post ([FromBody] CreateProductRequestDto productDto)
         {
             var productModel = productDto.ToProductFromCreateDto();
-            await _context.Products.AddAsync(productModel);
-            await _context.SaveChangesAsync();
+            await _productRepository.Post(productModel);
             return CreatedAtAction(nameof(GetById), new {id = productModel.Id}, productModel.ToProductDto());
         }
 
@@ -69,15 +70,12 @@ namespace ayudantis1.src.Controllers
 
         public async Task<IActionResult> Put([FromRoute] int id, [FromBody] UpdateProductRequestDto UpdateDto)
         {
-            var ProductModel = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+            var ProductModel = await _productRepository.Put(id, UpdateDto);
             if(ProductModel == null)
             {
                 return NotFound();
             }
-
-            ProductModel.Name = UpdateDto.Name;
-            ProductModel.Price = UpdateDto.Price;
-            await _context.SaveChangesAsync();
+        
             return Ok(ProductModel.ToProductDto());
             
         }
@@ -89,14 +87,12 @@ namespace ayudantis1.src.Controllers
 
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
-            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+            var product = await _productRepository.Delete(id);
             if(product == null)
             {
                 return NotFound();
             }
 
-            _context.Products.Remove(product);
-            await _context.SaveChangesAsync();
             return NoContent();
         }
 
